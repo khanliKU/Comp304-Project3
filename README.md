9014491af505479d38efd4344be42174d67b8371
Kutlay Hanli <khanli@ku.edu.tr>
2017-05-19 12:32:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Added bool type. “typedef enum { false, true } bool; “
+
+compare
+•	Started compare same as ls procedure. First thought about using ls() function but since it just prints the result and doesn’t hold them, decided to reflect it instead. 
+
+rename -used as rname because of conflict in some libraries-
---
+++ fs304.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stddef.h>
+#include <math.h>
+#include "fs304.h"
+
+int parseCommand(char inputBuffer[], char *args[]);
+
+int main(void){
+    char inputBuffer[50];
+    char *args[50];	        /* buffer to hold the command entered */
+    int shouldRun = 1;
+    mountFS304();     /* mount command to open fs304.disk */
+    
+    while(shouldRun){
+        shouldRun = parseCommand(inputBuffer, args);
+        if (strncmp(inputBuffer, "exit", 4) == 0){
+            shouldRun = 0;     /* sets*/
+        }
+        else if (strncmp(inputBuffer, "ls", 2) == 0){
+            ls();
+        }
+        else if (strncmp(inputBuffer, "md", 2) == 0){
+            md(args[1]);
+        }
+        else if (strncmp(inputBuffer, "cd", 2) == 0){
+            cd(args[1]);
+        }
+        else if (strncmp(inputBuffer, "stats", 5) == 0){
+            stats();
+        }
+        else if (strncmp(inputBuffer, "rd", 2) == 0){
+            rd();
+        }
+        else if (strncmp(inputBuffer, "compare", 7) == 0){
+            compare(args[1], args[2]);
+        }
+        else if (strncmp(inputBuffer, "rname", 5) == 0){
+            rname(args[1], args[2]);
+        } else {
+            printf("Unknown command!\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+int parseCommand(char inputBuffer[], char *args[])
+{
+    int numChars,		/* # of chars in command line */
+    i,          /* counter */
+    start,		/* beginning of next command */
+    ct,	        /* index of where to place the next parameter into args[] */
+    command_number;	/* index of requested command number */
+    
+    ct = 0;
+    
+    /* input reader */
+    do {
+        printPrompt();
+        fflush(stdout);
+        numChars = read(STDIN_FILENO,inputBuffer,50); /* writes input from file to inputBuffer. numChars = # of bytes read */
+    }
+    while (inputBuffer[0] == '\n');
+    
+    start = -1;
+    if (numChars == 0)
+        exit(0);            /* end of user command */
+    
+    
+    
+    /* Parse the contents of inputBuffer into args buffer */
+    
+    for (i=0;i<numChars;i++) {
+        switch (inputBuffer[i]){
+            case ' ':
+            case '\t' :               /* argument separators */
+                if(start != -1){
+                    args[ct] = &inputBuffer[start];    /* set up pointer */
+                    ct++;
+                }
+                inputBuffer[i] = '\0'; /* add a null char; make a C string */
+                start = -1;
+                break;
+                
+            case '\n':                 /* should be the final char examined */
+                if (start != -1){
+                    args[ct] = &inputBuffer[start];
+                    ct++;
+                }
+                inputBuffer[i] = '\0';
+                args[ct] = NULL; /* no more arguments to this command */
+                break;
+                
+            default :             /* some other character */
+                if (start == -1)
+                    start = i;
+        } /* end of switch */
+    }    /* end of for */
+    
+    return 1;
+    
+}
